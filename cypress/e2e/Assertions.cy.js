00b6cfb9e87650b7331fa58980b1eddc49be4a39
Pulimi Kanaka <pulimikanaka@Pulimis-MacBook-Pro.local>
2024-10-04 15:45:06 +0530

    added how you can use should and and in this

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -1,13 +1,22 @@
-const { describe } = require("mocha")
+
 
 
 describe ('Xpath',()=>{
     it('fImplicits assertions ',() => {
         cy.visit("https://qa.engati.com/admin/login")
 
+        //cy.url().should('include','qa.engati.com')
+        //cy.url().should('eq','https://qa.engati.com/admin/login')
+        //cy.url().should('contain','qa.engati')
+
+
+        //cy.url().should('include','qa.engati.com')
+        //.should('eq','https://qa.engati.com/admin/login')
+        //.should('contain','qa.engati')
+
         cy.url().should('include','qa.engati.com')
-        cy.url().should('eq','https://qa.engati.com/admin/login')
-        cy.url().should('contain','qa.engati')
+        .and('eq','https://qa.engati.com/admin/login')
+        .and('contain','qa.engati')
 
     })
 
